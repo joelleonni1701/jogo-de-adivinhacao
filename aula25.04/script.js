61561154f145cf4e5ffe0a08da3b4c728fcff6fa
vastolorde117 <joelleonni117@gmail.com>
2025-04-30 11:45:08 -0300

    atualização 30.04

--- aula25.04/script.js
+++ aula25.04/script.js
@@ -1,14 +1,14 @@
 //lista de palavras
 let palavras = [
-    {palavra:"video games",dicas:["bola 1", "pessoas 2","narrador 3"]},
-    {palavra:"melhoer do mundo",dicas:["estangeiro 1", "coroa portuguesa 2","eu sou melhor 3"]},
-    {palavra:"formula 1 ",dicas:["carros 1","velocidade 2","pilotos 3"]},
-    {palavra:"anime",dicas:["animação 1","história 2","poderes além dos seus limites 3 "]},
+    {palavra:"jogo de futebol",dicas:[" bola", " pessoas"," narrador"]},
+    {palavra:"cristiano ronaldo",dicas:["  estangeiro", " coroa portuguesa"," eu sou melhor"]},
+    {palavra:"formula 1",dicas:["carros"," velocidade"," pilotos"]},
+    {palavra:"anime",dicas:["animação ","história ","poderes além dos seus limites "]},
 ]
 
 
 //elementos 
-let input=document.getElementById("txt_input")
+let input = document.getElementById("txt_input")
 let div_resultado = document.getElementById("resultado")
 let paragrafo_dica = document.getElementById("paragrafo_dicas")
 let paragrafo_tentativas = document.getElementById("paragrafo_tentativas")
@@ -33,4 +33,8 @@ function adivinha(){
             paragrafo_tentativas.textContent = "Tentativas: " + tentativas
         } 
     }
+}
+function mostrar_dica(){
+    paragrafo_dica.textContent="Dica: "+ dicas[0]
+
 }
